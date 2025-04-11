eb2cb79f6b0cf3de7ae7b24deac3b9b7ffb5b4eb
Karan <karan.ekarigar@gmail.com>
2025-04-11 17:52:59 +0530

    start footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
   standalone:true,
+  imports: [IonicModule,
+    CommonModule,
+  RouterModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
